c3f3cfe4b2e32a9da94260429b82f9b3df916d98
Mohammad Fattouh <mo.fattouh@gmail.com>
2018-03-16 17:31:59 +0100

    refactor code. add more options

--- pytorch_util.py
+++ pytorch_util.py
@@ -4,42 +4,51 @@ from torch.utils.data import Dataset
 from torch.autograd import Variable
 
 
+def load_trial(dataset_file, trial, full_load=True):
+    trial = dataset_file[trial]
+    if full_load:
+        X = trial['X'][:]
+        y = trial['y'][:]
+    else:
+        X = trial['X']
+        y = trial['y']
+
+    return X, y, trial.attrs['srate'], trial.attrs['gameType']
+
+
 class ECoGDatast(Dataset):
-    def __init__(self, dataset_file, trial, window=1, stride=1, flatten=True, full_load=True):
-        # this loads all the trials into memory
-        self._trial = dataset_file[trial]
-        if full_load:
-            self._X = self._trial['X'][:]
-            self._y = self._trial['y'][:]
-        else:
-            self._X = self._trial['X']
-            self._y = self._trial['y']
-        assert self._X.shape[-1] == self._y.shape[0]
-        self._num_samples = self._X.shape[-1]
-        self._game_type = self._trial.attrs['gameType']
-        self._srate = self._trial.attrs['srate']
-        self._window = window
-        self._stride = stride
-        self._flatten = flatten
+    def __init__(self, X, y, window=1, stride=1, x2y_ratio=1):
+        '''
+        param: X is an nd array with first dim is time axis
+        y is
+        '''
+        self._xwindow = window
+        self._ywindow = int(window / x2y_ratio)
+        self._xstride = stride
+        self._ystride = int(stride / x2y_ratio)
+        self._num_samples = int((X.shape[0] - self._xwindow) / self._xstride) + 1
+        assert self._num_samples <= int((y.shape[0] - self._ywindow) / self._ystride) + 1,\
+            'number of examples X (%d) does not match number of labels y(%d)' % (self._num_samples, int((y.shape[0] - self._ywindow) / self._ystride) + 1)
+        self._X = X
+        self._y = y
 
     def __len__(self):
-        return int((self._num_samples - self._window) / self._stride) + 1
+        return self._num_samples
 
     def __getitem__(self, idx):
-        X = self._X[:, :, idx * self._stride:idx * self._stride + self._window].squeeze().astype(np.float32)
-        y = self._y[idx * self._stride:idx * self._stride + self._window].squeeze().astype(np.float32)
-        if self._flatten:
-            X = X.reshape((-1, self._window))
+        X = self._X[idx * self._xstride: idx * self._xstride + self._xwindow].squeeze().astype(np.float32)
+        y = self._y[idx * self._ystride: idx * self._ystride + self._ywindow].squeeze().astype(np.float32)
+        if y.ndim < 2:
+            y = np.expand_dims(y, axis=1)
         return X, y
-
-    def srate(self):
-        return self._srate
 
 
 class ConcatDataset(Dataset):
-    def __init__(self, datasets):
+    def __init__(self, datasets, batch_first=False, time_last=False):
         self._len = min([len(dataset) for dataset in datasets])
         self.datasets = list(datasets)
+        self.batch_first = batch_first
+        self.time_last = time_last
 
     def __len__(self):
         return self._len
@@ -47,45 +56,21 @@ class ConcatDataset(Dataset):
     def __getitem__(self, idx):
         X = []
         y = []
-        for dataset in self.datasets:
-            dataset_X, dataset_y = dataset.__getitem__(idx)
-            X.append(np.expand_dims(dataset_X, axis=0))
-            y.append(np.expand_dims(dataset_y, axis=0))
-        return np.concatenate(X, axis=0).astype(np.float32), np.concatenate(y, axis=0).astype(np.float32)
+        if self.batch_first:
+            for dataset in self.datasets:
+                dataset_X, dataset_y = dataset.__getitem__(idx)
+                if self.time_last:
+                   dataset_X = dataset_X.transpose()
 
-
-def train(model, data_loader, optimizer, loss_fun, cuda=False):
-    model.train()
-    for data, target in data_loader:
-        if cuda:
-            data, target = Variable(data.squeeze().cuda()), Variable(target.squeeze().cuda())
-            # might be helpfull to clear cache
-            torch.cuda.empty_cache()
+                X.append(np.expand_dims(dataset_X, axis=0))
+                y.append(np.expand_dims(dataset_y, axis=0))
+            return np.concatenate(X, axis=0).astype(np.float32), np.concatenate(y, axis=0).astype(np.float32)
         else:
-            data, target = Variable(data.squeeze()), Variable(target.squeeze())
-        optimizer.zero_grad()
-        output = model(data).squeeze()
-        loss = loss_fun(output, target[:, -output.size()[-1]:])
-        loss.backward()
-        optimizer.step()
+            for dataset in self.datasets:
+                dataset_X, dataset_y = dataset.__getitem__(idx)
+                if self.time_last:
+                    dataset_X = dataset_X.transpose()
+                X.append(np.expand_dims(dataset_X, axis=1))
+                y.append(np.expand_dims(dataset_y, axis=0))
+            return np.concatenate(X, axis=1).astype(np.float32), np.concatenate(y, axis=0).astype(np.float32)
 
-
-def metrics(model, data_loader, loss_fun, writer, epoch, cuda):
-    model.eval()
-    # loop over the training dataset
-    avg_corr = []
-    avg_loss = 0
-    for data, target_cpu in data_loader:
-        if cuda:
-            data, = Variable(data.squeeze().cuda(), volatile=True)
-            target = Variable(target_cpu.squeeze().cuda(), volatile=True)
-        else:
-            data, target = Variable(data.squeeze()), Variable(target_cpu.squeeze())
-        output = model(data).squeeze()
-        avg_corr += np.corrcoef(target_cpu[:, -output.size()[-1]:].data.numpy().squeeze(),
-                                output.data.cpu().numpy().squeeze())[0, 1]
-        avg_loss += loss_fun(output, target[:, -output.size()[-1]:])
-
-    writer.add_scalar('corr', avg_corr / len(data_loader.dataset), epoch)
-    writer.add_scalar('loss', avg_loss / len(data_loader.dataset), epoch)
-    return avg_loss, avg_corr
