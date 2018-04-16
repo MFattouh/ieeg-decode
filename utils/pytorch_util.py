18dbca31d13bee3abc7e7565ed69df4e929fe9ea
Mohammad Fattouh <mo.fattouh@gmail.com>
2018-04-16 17:33:14 +0200

    validate input

--- utils/pytorch_util.py
+++ utils/pytorch_util.py
@@ -112,33 +112,50 @@ def load_trial(dataset_file, trial, full_load=True):
         X = trial['X']
         y = trial['y']
 
-    return X, y, trial.attrs['srate'], trial.attrs['gameType']
+    return X, y, trial.attrs['srate']
 
 
 class ECoGDatast(Dataset):
-    def __init__(self, X, y, window=1, stride=1, x2y_ratio=1, time_last=True, dummy_idx=0):
+    def __init__(self, X, y, window=1, stride=1, x2y_ratio=1, input_shape='tc', time_last=True, dummy_idx='f'):
         '''
         param: X is an nd array with first dim is time axis
         y is
         '''
+        self.input_shape = input_shape.lower()
+        assert self.input_shape in ['ct', 'tc'], 'input shape "%s" not understood. Only "CT" and "TC" are supported'\
+                                                 % input_shape
+        if (self.input_shape[-1] == 't' and X.shape[-1] < window) or \
+                (self.input_shape[0] == 't' and X.shape[-1] < window):
+            raise ValueError('No enough samples for one window!')
+
+        assert dummy_idx.lower() in ['f', 'l'], 'dummy index "%s" not understood. Only f(first) ofr l(last) are ' \
+                                                'supported' % dummy_idx
+        self.dummy_idx = 0 if dummy_idx.lower() == 'f' else 2
         self._xwindow = window
         self._ywindow = int(window / x2y_ratio)
         self._xstride = stride
         self._ystride = int(stride / x2y_ratio)
-        self._num_samples = int((X.shape[0] - self._xwindow) / self._xstride) + 1
+        if self.input_shape[-1] == 'c':
+            self._num_samples = int((X.shape[0] - self._xwindow) / self._xstride) + 1
+        else:
+            self._num_samples = int((X.shape[-1] - self._xwindow) / self._xstride) + 1
         assert self._num_samples <= int((y.shape[0] - self._ywindow) / self._ystride) + 1, \
-            'number of examples X (%d) does not match number of labels y(%d)' % (self._num_samples, int((y.shape[0] - self._ywindow) / self._ystride) + 1)
+            'number of examples X (%d) does not match number of labels y(%d)' %\
+            (self._num_samples, int((y.shape[0] - self._ywindow) / self._ystride) + 1)
         self._X = X
         self._y = y
         self.time_last = time_last
-        self.dummy_idx = dummy_idx
 
     def __len__(self):
         return self._num_samples
 
     def __getitem__(self, idx):
-        X = self._X[idx * self._xstride: idx * self._xstride + self._xwindow].squeeze().astype(np.float32)
-        X = np.expand_dims(X.T if self.time_last else X, axis=self.dummy_idx).astype(np.float32)
+        if self.input_shape[-1] == 'c':
+            X = self._X[idx * self._xstride: idx * self._xstride + self._xwindow,:].squeeze().astype(np.float32)
+            X = np.expand_dims(X.T if self.time_last else X, axis=self.dummy_idx).astype(np.float32)
+        else:
+            X = self._X[:, idx * self._xstride: idx * self._xstride + self._xwindow].squeeze().astype(np.float32)
+            X = np.expand_dims(X if self.time_last else X.T, axis=self.dummy_idx).astype(np.float32)
         y = self._y[idx * self._ystride: idx * self._ystride + self._ywindow].squeeze().astype(np.float32)
         return X, y
 
