8d86503075f4f4aad6f182c9f26e4819bcd3edea
Mohammad Fattouh <mo.fattouh@gmail.com>
2018-04-25 16:41:04 +0200

    visualize and compare folds

---
+++ visualization/folds_bars.py
@@ -0,0 +1,38 @@
+# creates a bar plot of cross-validation folds for different experiments from csv files
+
+import os
+import pandas as pd
+from glob import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+import click
+
+
+@click.command()
+@click.argument('dataset_dir', type=click.Path(exists=True))
+def plot_folds(dataset_dir):
+    csv_pathes = glob(os.path.join(dataset_dir, '*/*.csv'))
+    assert csv_pathes, 'No csv files found!'
+    experiments = [os.path.basename(os.path.dirname(path)) for path in csv_pathes]
+    subject = os.path.basename(os.path.basename(csv_pathes[0]))
+    df_list = []
+    for experiment, csv_path in zip(experiments, csv_pathes):
+        df = pd.read_csv(csv_path)
+        df.loc[:, 'exp'] = experiment
+        df.sort_values(['day', 'exp'], inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        df_list.append(df)
+    results = pd.concat(df_list)
+    results.reset_index(drop=True, inplace=True)
+    days = list(set(results.day.values.tolist()))
+    n_folds = len(set(df.fold.values.tolist()))
+    for day in days:
+        gp = results[results.day == day]
+        sns.factorplot(x='fold', y='corr', hue='exp', kind='bar', size=6, data=gp,
+                       order=['fold%d' % fold for fold in range(1, n_folds+1)]
+                       , hue_order=['1Layer', '2Layers', '3Layers'])  # needed to order the layers
+        plt.savefig(os.path.join(dataset_dir, day+'.png'))
+
+
+if __name__ == '__main__':
+    plot_folds()
