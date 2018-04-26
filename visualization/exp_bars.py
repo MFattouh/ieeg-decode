304e22e49ef56053fd7e797eae1d7ffaa4de03a2
Mohammad Fattouh <mo.fattouh@gmail.com>
2018-04-26 14:00:12 +0200

    bar plots per experiment setup

---
+++ visualization/exp_bars.py
@@ -0,0 +1,28 @@
+# creates a bar plot to compare the average performance per experiment configuration
+
+import os
+import pandas as pd
+from glob import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+import click
+import numpy as np
+
+@click.command()
+@click.argument('dataset_dir', type=click.Path(exists=True))
+def plot_folds(dataset_dir):
+    csv_pathes = glob(os.path.join(dataset_dir, '*/*/*.csv'))
+    assert csv_pathes, 'No csv files found!'
+    experiments = [os.path.basename(os.path.dirname(path)) for path in csv_pathes]
+    results = pd.DataFrame(index=np.arange(len(csv_pathes)), columns=['corr', 'exp'])
+    for idx, (experiment, csv_path) in enumerate(zip(experiments, csv_pathes)):
+        df = pd.read_csv(csv_path)
+        results.loc[idx, :] = [df['corr'].mean(), experiment]
+
+    sns.factorplot(x='exp', y='corr', kind='bar', size=6, data=results)
+
+    plt.savefig(os.path.join(dataset_dir, os.path.basename(os.path.dirname(dataset_dir)) + '.png'))
+
+
+if __name__ == '__main__':
+    plot_folds()
