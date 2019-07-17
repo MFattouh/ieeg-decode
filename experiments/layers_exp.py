73aaf835fe629eda5739df96cd090c3310a8692e
Mohammad Fattouh <mo.fattouh@gmail.com>
2019-07-17 16:46:10 +0200

    update layers experiments

--- experiments/layers_exp.py
+++ experiments/layers_exp.py
@@ -5,69 +5,77 @@ from utils.experiment_util import *
 from tensorboardX import SummaryWriter
 from torch import optim
 import h5py
+import yaml
 from glob import glob
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, LeaveOneOut, train_test_split
 import click
 import pandas as pd
+from utils.config import cfg, merge_configs
+import random
+import json
 
-MAX_EPOCHS = 1000
-EVAL_EVERY = 50
 CUDA = True
-EXPERIMENT_NAME = 'layers'
-RANDOM_SEED = 10418
-np.random.seed(RANDOM_SEED)
-torch.manual_seed(RANDOM_SEED)
+TASK_NAMES = ['XPOS', 'XVEL', 'ABSPOS', 'ABSVEL']
 
+logger = logging.getLogger(__name__)
 
 @click.command()
+@click.argument('configs', type=click.Path(), default=os.path.curdir)
 @click.argument('dataset_dir', type=click.Path(exists=True))
 @click.argument('subject', type=str)
 @click.argument('log_dir', type=click.Path())
 @click.argument('num_layers', type=int)
 @click.option('--n_splits', default=5, help='Number of cross-validation splits')
-def main(dataset_dir, subject, log_dir, num_layers, n_splits):
+@click.option('--task', '-t', type=click.Choice(['xpos', 'xvel', 'abspos', 'absvel', 'multi']), default='xpos',
+              help='Task to decode. acceptable are:\n'
+                   '* xpos for position decoding.\n'
+                   '* xvel for velocity decoding.\n'
+                   '* abspos for absolute position decoding.\n'
+                   '* absvel for absolute velocity decoding.\n'
+                   '* multi for multi-task decoding.\n'
+                   'default is pos')
+def main(configs, dataset_dir, subject, log_dir, num_layers, n_splits, task):
+    with open(configs, 'r') as f:
+        merge_configs(yaml.load(f))
+        assert cfg.TRAINING.MODEL == 'RNN'
+        assert cfg.HYBRID.RNN.NUM_LAYERS == num_layers
+
+    # set the random state
+    np.random.seed(cfg.TRAINING.RANDOM_SEED)
+    torch.manual_seed(cfg.TRAINING.RANDOM_SEED)
+    random.seed(cfg.TRAINING.RANDOM_SEED)
+
     if num_layers == 1:
-        log_dir = os.path.join(log_dir, EXPERIMENT_NAME, subject, str(num_layers)+'Layer')
+        log_dir = os.path.join(log_dir, task.upper(), subject, '1Layer')
     else:
-        log_dir = os.path.join(log_dir, EXPERIMENT_NAME, subject, str(num_layers)+'Layers')
+        log_dir = os.path.join(log_dir, task.upper(), subject, str(num_layers) + 'Layers')
     if not os.path.isdir(log_dir):
         os.makedirs(log_dir)
 
-    datasets = glob(dataset_dir + '*'+subject+'_*_xpos.mat')
-    assert len(datasets) > 0, 'no datasets for subject %s found!' % subject
-    new_srate_x = 250
-    new_srate_y = 250
-    x2y_ratio = new_srate_x / new_srate_y
-    batch_size = 32
-    hidden_size = 64
-    channel_config = None
-    time_config = None
-    weights_dropout = []
-    if num_layers > 1:
-        weights_dropout.extend(['weight_ih_l%d' % layer for layer in range(num_layers)])
-        weights_dropout.extend(['weight_hh_l%d' % layer for layer in range(num_layers)])
-    rnn_config = {'rnn_type': 'gru',
-                  'hidden_size': hidden_size,
-                  'num_layers': num_layers,
-                  'dropout': 0.3,
-                  'weights_dropout': weights_dropout,
-                  'batch_norm': False}
-    fc_config = {'num_classes': 1,
-                 'fc_size': [32, 10],
-                 'batch_norm': [False, False],
-                 'dropout': [0.5, .3],
-                 'activations': [nn.Hardtanh(-1, 1, inplace=True)] * 2}
+    logging.basicConfig(level=logging.DEBUG, filename=os.path.join(log_dir, 'log.txt'), filemode='w+',
+                        format='%(levelname)s %(filename)s:%(lineno)4d: %(message)s')
+    logger.info('Called with configs:')
+    logger.info(json.dumps(cfg, indent=2))
 
-    # window size
-    crop_len = 16
-    num_relaxed_samples = 681  # int(relax_window * new_srate_x)
-    learning_rate = 5e-3
-    wd_const = 5e-6
-    stride = crop_len * new_srate_x - num_relaxed_samples
-    weights = make_weights(crop_len * new_srate_x, num_relaxed_samples, type='step')
-    weights_tensor = torch.from_numpy(weights)
-    if CUDA:
-        weights_tensor = weights_tensor.cuda()
+    datasets = []
+    if task == 'xpos':
+        datasets = glob(dataset_dir + '*' + subject+'_*_xpos.mat')
+    elif task == 'xvel':
+        datasets = glob(dataset_dir + '*' + subject+'_*_xvel.mat')
+    elif task == 'abspos':
+        datasets = glob(dataset_dir + '*' + subject+'_*_absPos.mat')
+    elif task == 'absvel':
+        datasets = glob(dataset_dir + '*' + subject+'_*_absVel.mat')
+    elif task == 'multi':
+        pos_datasets = glob(dataset_dir + '*' + subject+'_*_xpos.mat')
+        for pos_dataset in pos_datasets:
+            recording_day = pos_dataset.rstrip('_xpos.mat')
+            vel_dataset = recording_day + '_xvel.mat'
+            assert os.path.exists(vel_dataset), 'could not find enough files for multi-task decoding'
+            datasets.append((pos_dataset, vel_dataset))
+    else:
+        raise KeyError
+    assert len(datasets) > 0, 'no datasets for subject %s found!' % subject
 
     # data frame to hold cv cross. corr.
     rec_names = []
@@ -79,56 +87,61 @@ def main(dataset_dir, subject, log_dir, num_layers, n_splits):
                                        names=['day', 'fold'])
     df = pd.DataFrame(index=index, columns=['corr', 'mse'])
     for dataset_path, rec_name in zip(datasets, rec_names):
-        msg = str(datetime.now()) + ': Start working on dataset %s:' % dataset_path
-        print(msg)
-        print('='*len(msg))
-        print('='*len(msg))
-        crops, in_channels = read_dataset(dataset_path, crop_len*new_srate_x, stride, 'f')
-        print(len(crops), 'trials found!')
-        crop_idx = np.arange(len(crops)).squeeze().tolist()
-        kfold = KFold(n_splits=n_splits, shuffle=False, random_state=RANDOM_SEED)
-        for cross_valid_idx, (train_split, valid_split) in enumerate(kfold.split(crop_idx), 1):
-            training_loader, valid_loader = create_loader(crops, train_split, valid_split, batch_size)
-            msg = str(datetime.now()) + ': FOLD%d:' % cross_valid_idx
-            print(msg)
-            print('='*len(msg))
-            print('Training trials:')
-            print(train_split)
-            print('Validation trials:')
-            print(valid_split)
-            model = HybridModel(in_channels=in_channels, channel_conv_config=channel_config,
-                                time_conv_config=time_config, rnn_config=rnn_config,
-                                fc_config=fc_config, output_stride=int(x2y_ratio))
+        msg = str('Working on dataset %s:' % rec_name if task == 'multi' else dataset_path)
+        logger.info(msg + '\n' + '=' * len(msg))
+        dataset_name = 'D'
+        if task == 'multi':
+            trials, in_channels = read_multi_datasets(dataset_path, dataset_name, mha_only=cfg.TRAINING.MHA_CHANNELS_ONLY)
+            num_classes = len(dataset_path)
+        else:
+            trials, in_channels = read_dataset(dataset_path, dataset_name, mha_only=cfg.TRAINING.MHA_CHANNELS_ONLY)
+            num_classes = 1
+        logger.info(f'{len(trials)} trials found')
+        logger.info(f'Number of input input channels: {in_channels}')
+        if in_channels < 1:
+            logger.warning(f'Zero valid channels found!!!!!!')
+            print(f'Zero valid channels found!!!!!!')
+            return
 
-            if CUDA:
-                model.cuda()
+        crop_idx = list(range((len(trials))))
+        if n_splits > 0:
+            kfold = KFold(n_splits=n_splits, shuffle=False, random_state=cfg.TRAINING.RANDOM_SEED)
+        elif n_splits == -1:
+            kfold = LeaveOneOut()
+        else:
+            raise ValueError(f'Invalid number of splits: {n_splits}')
 
-            optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=wd_const)
-            loss_fun = WeightedMSE(weights_tensor)
-            metric = CorrCoeff(weights).weighted_corrcoef
+        for fold_idx, (train_split, valid_split) in enumerate(kfold.split(crop_idx), 1):
 
-            # scheduler = StepLR(optimizer, step_size=100, gamma=0.9)
-            training_writer = SummaryWriter(os.path.join(log_dir, rec_name, 'fold' + str(cross_valid_idx), 'train'))
-            valid_writer = SummaryWriter(os.path.join(log_dir, rec_name, 'fold' + str(cross_valid_idx), 'valid'))
-            # training_writer.add_text('Model parameters', str(HybridModel.get_meta(model)))
-            training_writer.add_text('Description', 'RNN with %s layer(s)' % num_layers)
-            training_writer.add_text('Learning Rate', str(learning_rate))
-            training_writer.add_text('Weight Decay', str(wd_const))
-            training_writer.add_text('Crop Length[sec]', str(crop_len))
-            training_writer.add_text('Input srate[Hz]', str(new_srate_x))
-            training_writer.add_text('Output srate[Hz]', str(new_srate_y))
-            training_writer.add_text('relaxed samples', str(num_relaxed_samples))
-            training_writer.add_text('Input channels', str(in_channels))
+            model, optimizer, scheduler, loss_fun, metric = create_model(in_channels, num_classes, CUDA)
 
-            weights_path = os.path.join(log_dir, rec_name, 'fold' + str(cross_valid_idx), 'weights.pt')
+            msg = str(f'FOLD{fold_idx}:')
+            logger.info(msg)
+            logger.info('='*len(msg))
+            logger.info('Training trials:')
+            logger.info(train_split)
+            logger.info('Validation trials:')
+            logger.info(valid_split)
 
-            fold_corr, fold_mse = run_experiment(model, optimizer, loss_fun, metric, training_loader, training_writer,
-                                                 valid_loader, valid_writer, weights_path, max_epochs=MAX_EPOCHS,
-                                                 eval_train_every=EVAL_EVERY, eval_valid_every=EVAL_EVERY, cuda=CUDA)
+            training_writer = SummaryWriter(os.path.join(log_dir, rec_name, 'fold' + str(fold_idx), 'train'))
+            valid_writer = SummaryWriter(os.path.join(log_dir, rec_name, 'fold' + str(fold_idx), 'valid'))
+            weights_path = os.path.join(log_dir, rec_name, 'fold' + str(fold_idx), 'weights.pt')
 
-            df.loc[(rec_name, 'fold' + str(cross_valid_idx)), :] = [fold_corr, fold_mse]
+            training_trials = [trials[idx] for idx in train_split]
+            valid_trials = [trials[idx] for idx in valid_split]
+            corr, mse = training_loop(model, optimizer, scheduler, loss_fun, metric, training_trials,
+                                        training_writer, valid_trials, valid_writer, weights_path, cuda=CUDA)
+            if task == 'multi':
+                for task_idx in range(len(corr)):
+                    df.loc[(rec_name, 'fold' + str(fold_idx)), TASK_NAMES[task_idx]] = \
+                        corr['Class%d' % task_idx]
+                df.loc[(rec_name, 'fold' + str(fold_idx)), 'mse'] = mse
+            else:
+                df.loc[(rec_name, 'fold' + str(fold_idx)), :] = [corr, mse]
             # writes every time just in case it couldn't run the complete script
             df.to_csv(os.path.join(log_dir, 'cv_acc.csv'), index=True)
+
+    print("Done!")
 
 
 if __name__ == '__main__':
